84f47024f7e00df5fef7f7be6dbcf5c570c24564
NaStyA128 <n.a.s.t.y.a28v@gmail.com>
2016-06-15 11:58:34 +0300

    New function with rays diagram

--- new_diagrams.py
+++ new_diagrams.py
@@ -14,9 +14,12 @@ class Diagrams:
     input_text = ""
     legend_start_point_y = 200
 
-    def __init__(self, text, my_turtle):
+    def __init__(self, text, my_turtle, type):
         self.input_text = text
-        self.__sector__(my_turtle)
+        if type == 0:
+            self.__sector__(my_turtle)
+        else:
+            self.__rays__(my_turtle)
 
     def __sector__(self, my_turtle):
         words = re.sub('\W', ' ', self.input_text).split()
@@ -56,11 +59,46 @@ class Diagrams:
         my_turtle.penup()
         my_turtle.setpos(0, 0)
 
+    def __rays__(self, my_turtle):
+        words = re.sub('\W', ' ', self.input_text).split()
+        count_word = Counter(words)
+        quantity_type_words = len(count_word)
+        angle = 360 / quantity_type_words
+
+        for c in count_word:
+            j = 1
+            my_turtle.pendown()
+            my_turtle.pencolor(random.choice(self.COLORS))
+            while j <= count_word[c]:
+                my_turtle.forward(50)
+                my_turtle.circle(2)
+                j += 1
+            my_turtle.penup()
+            position = my_turtle.pos()
+            if position[1] > 5.00:
+                my_turtle.goto(my_turtle.xcor(), my_turtle.ycor() + 10)
+            elif -5.00 < position[1] < 5.00:
+                if position[0] < -5.00:
+                    my_turtle.goto(my_turtle.xcor() - 50, my_turtle.ycor())
+                elif position[0] > 5.00:
+                    my_turtle.goto(my_turtle.xcor() + 20, my_turtle.ycor())
+            else:
+                my_turtle.goto(my_turtle.xcor(), my_turtle.ycor() - 20)
+            my_turtle.pendown()
+            my_turtle.write(c)
+            my_turtle.penup()
+            my_turtle.goto(0, 0)
+            my_turtle.left(angle)
+        my_turtle.hideturtle()
+
 
 def main():
+    print "Input type (0 - sector, 1 - rays)"
+    a = input()
+    a = int(a)
     turt = turtle.Turtle()
     turt.speed(10)
-    Diagrams("My name is Nastya. My cat is cool. It is funny.", turt)
+    Diagrams("My name is Nastya. My cat is cool. It is funny and nice.", turt, a)
     turtle.done()
 
 if __name__ == "__main__":
